a59b9f580e701f23f98ac99139d3b20359f58896
ShadowTune <rifatphysicist1@outlook.com>
2025-03-20 22:37:27 +0600

    migration issue

--- faucet/src/App.js
+++ faucet/src/App.js
@@ -1,59 +1,85 @@
-
 import { useEffect, useState } from "react";
 import "./App.css";
 import Web3 from "web3";
-
+import detectEthereumProvider from '@metamask/detect-provider'
+import { loadContract } from "./utils/load-contract";
 
 function App() {
-  const [web3Api, setWeb3Api] = useState({
-    provider: null,
-    web3: null
-  })
+	const [web3Api, setWeb3Api] = useState({
+		provider: null,
+		web3: null,
+		contract: null
+	})
 
-  useEffect(() => {
-      const loadProvider = async () => {
-      let provider = null;
+	const [account, setAccount] = useState(null)
 
-      if (window.ethereum) {
-        provider = window.ethereum;
+	useEffect(() => {
+		const loadProvider = async () => {
+			const provider = await detectEthereumProvider();
+			const contract = await loadContract("Faucet");
+			debugger
+			if (provider) {
+				// provider.request({ method: "eth_requestAccounts" })
+				setWeb3Api({
+					web3: new Web3(provider),
+					provider,
+					contract
+				})
+			} else {
+				console.error("Access denied")
+			}
+		}
+		loadProvider()
+	}, [])
 
-        try {
-          await provider.enable();
-        } catch {
-          console.error("User denied accounts access!")
-        }
-      }
-      else if (window.web3) {
-        provider = window.web3.currentProvider
-      }
-      else if (!process.env.production) {
-        provider = new Web3.providers.HttpProvider("http://localhost:8545")
-      }
+	useEffect(() => {
+		const getAccounts = async () => {
+			const accounts = await web3Api.web3.eth.getAccounts();
+			setAccount(accounts[0]);
+		}
+		web3Api.web3 && getAccounts()
+	}, [web3Api.web3])
 
-      setWeb3Api({
-        web3: new Web3(provider),
-        provider
-      })
-    }
 
-    loadProvider()
-  }, [])
+	console.log(web3Api.web3);
 
-  console.log(web3Api.web3)
-
-  return (
-    <>
-      <div className="faucet-wrapper">
-        <div className="faucet">
-          <div className="balance-view is-size-2">
-            Current Balance: <strong>10</strong> ETH
-          </div>
-          <button className="btn mr-2">Donate</button>
-          <button className="btn">Withdraw</button>
-        </div>
-      </div>
-    </>
-  );
+	return (
+		<>
+			<div className="faucet-wrapper">
+				<div className="faucet">
+					<div className="is-flex is-align-items-center">
+						<span>
+							<strong className="mr-2">Account: </strong>
+						</span>
+						{
+							account ? <div>{account}</div> :
+								<button lassName="button is-small"
+									onClick={() => web3Api.provider.request({
+										method: "eth_requestAccounts"
+									})}> Connect Wallet
+								</button>
+						}
+					</div>
+					<div className="balance-view is-size-2 my-4">
+						Current Balance: <strong>10</strong> ETH
+					</div>
+					{/* <button
+						className="btn mr-2"
+						onClick={async () => {
+						const accounts = await window.ethereum.request({method: "eth_requestAccounts"})
+						console.log(accounts)
+						}}
+						>
+						Enable Ethereum
+					</button> */}
+					<button
+						className="button is-link mr-2">Donate</button>
+					<button
+						className="button is-primary">Withdraw</button>
+				</div>
+			</div>
+		</>
+	);
 }
 
 export default App;
